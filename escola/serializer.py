cffc94027d5c3db4c649f8ae729145fdbaef8c6b
Julio Felipe <julio.felipe@casaldev.com.br>
2020-08-13 18:02:30 -0300

    course finish

--- escola/serializer.py
+++ escola/serializer.py
@@ -17,6 +17,16 @@ class MatriculaSerializer(serializers.ModelSerializer):
     exclude = []
 
 class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+  curso = serializers.ReadOnlyField(source='curso.descricao')
+  periodo = serializers.SerializerMethodField()
   class Meta:
     model = Matricula
     fields = ['curso', 'periodo']
+  def get_periodo(self, obj):
+    return obj.get_periodo_display()
+
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+  aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
+  class Meta:
+    model = Matricula
+    fields = ['aluno_nome']
